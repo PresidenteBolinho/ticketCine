bc4ca211d33247879cae5d7ddf99cfd67900ae69
Gustavo Bolinho <gustavospr@hotmail.com>
2021-12-21 11:40:22 -0300

    Configuração das rotas para FilmsDetails e Ticket

--- ticketcine/src/App.vue
+++ ticketcine/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
+    <router-link to="/film">Film</router-link> |
+    <router-link to="/ticket">Tickets</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
