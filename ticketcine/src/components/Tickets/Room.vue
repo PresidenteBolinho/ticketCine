e3e6fe552bbd8cabf8aa188c37a20ef2065aa142
Gustavo Bolinho <gustavospr@hotmail.com>
2021-12-21 11:32:59 -0300

    Componentização de sala.html e checkout.html em: Tickets, Checkout e Room

---
+++ ticketcine/src/components/Tickets/Room.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="room">
+    <h2>Sua cadeira é a:</h2>
+    <section class="places">
+      <table>
+        <title>Sala 1</title>
+        <label>Tela</label>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Número 1</th>
+            <th>Número 2</th>
+            <th>Número 3</th>
+            <th>Número 4</th>
+            <th>Número 5</th>
+            <th>Número 6</th>
+            <th>Número 7</th>
+            <th>Número 8</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Letra A</td>
+          </tr>
+          <tr>
+            <td>Letra B</td>
+          </tr>
+          <tr>
+            <td>Letra C</td>
+          </tr>
+          <tr>
+            <td>Letra D</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>D4</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Letra E</td>
+          </tr>
+          <tr>
+            <td>Letra F</td>
+          </tr>
+          <tr>
+            <td>Letra G</td>
+          </tr>
+          <tr>
+            <td>Letra H</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    <div>
+      <button>Voltar</button>
+      <button>Próximo</button>
+    </div>
+  </section>
+</template>
---
+++ ticketcine/src/views/Tickets.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Room />
+    <Checkout />
+  </div>
+</template>
+
+<script>
+import Room from "@/components/Tickets/Room.vue";
+import Checkout from "@/components/Tickets/Checkout.vue";
+
+export default {
+  name: "Tickets",
+  components: {
+    Room,
+    Checkout,
+  },
+};
+</script>
